532f79055aec33ea4b2ea2e47b9dda38f381ce3d
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-09-23 10:56:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php 
 $amount = filter_input(INPUT_POST, 'amount');
 $final = 0 ;
-define('EUR_CZK',25);
-define('CZK_EUR',1);
-define('USD_CZK',23);
-define('CZK_USD',1);
-define('RUB_CZK',0.30);
-define('CZK_RUB',1);
-define('GBP_CZK',30);
-define('CZK_GBP',1);
+define('EUR_CZK', 25);
 $sub = filter_input(INPUT_POST, 'odeslat');
 $switch =  filter_input(INPUT_POST, 'switch');
 $curencyfrom;
 $curencyto;
 $text = "Převod je hotov : " ;
 ?>
-<br>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,6 +18,7 @@ $text = "Převod je hotov : " ;
 </head>
 <body>
 <h1>Směnárna</h1>
+<br>
 <?php
 if (isset($sub)) {
 
@@ -33,60 +27,23 @@ if (isset($sub)) {
         $final = $amount / EUR_CZK ;
         $curencyfrom = " CZK " ;
         $curencyto = " EUR " ;
-             break;
+        break;
     case 'eur_czk': 
         $final = $amount * EUR_CZK ;
         $curencyfrom = " EUR " ;
         $curencyto = " CZK " ;
-            break;
-     case 'czk_usd': 
-        $final = $amount / USD_CZK ;
-        $curencyfrom = " CZK " ;
-        $curencyto = " USD " ;
-            break;
-    case 'usd_czk': 
-         $final = $amount / USD_CZK ;
-         $curencyfrom = " USD " ;
-         $curencyto = " CZK " ;
-            break;
-    case 'czk_rub': 
-            $final = $amount / RUB_CZK ;
-            $curencyfrom = " CZK " ;
-            $curencyto = " RUB " ;
-            break;
-     case 'rub_czk': 
-            $final = $amount / RUB_CZK ;
-            $curencyfrom = " RUB " ;
-            $curencyto = " CZK " ;
-            break;
+        break;
                     }
 
         $all =$text . $amount . $curencyfrom . " = " . $final . $curencyto ?>
 <?= $all ?>
 <?php
 } else { ?>
-     <label for="amount">Vyberte z čeho chcete převádět : </label>
-
-       <select name="amount" id="amount">
-       <option value="none">Měna...</option>
-          <option value="CZK">Česká koruna</option>
-          <option value="GBP">Libra</option>
-          <option value="USD">Dollar</option>
-         <option value="RUB">Rubl</option>
-        </select> z
-        <select name="amount" id="amount">
-       <option value="none">Měna...</option>
-          <option value="CZK">Česká koruna</option>
-          <option value="GBP">Libra</option>
-          <option value="USD">Dollar</option>
-         <option value="RUB">Rubl</option>
-        </select>
-        <br>
-        <br>
-        <form action="index.php" method="post">
-
+    <form action="index.php" method="post">
 Peníze: <input type="number" name="amount" id="amount"> <br>
      <br>
+        CZK to EUR: <input type="radio" name="switch" value="czk_eur" id="switch"><br>
+        EUR to CZK: <input type="radio" name="switch" value="eur_czk" id="switch"><br>
         <br>
         <input type="submit" value="odeslat" name="odeslat">
     </form>
